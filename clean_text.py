ad68d5a872dc4fb03dab851a0e2b8377f5aceafc
RINKI NAG <rinkinag24@gmail.com>
2023-08-30 18:32:14 +0530

    Update clean_text.py

--- clean_text.py
+++ clean_text.py
@@ -5,7 +5,7 @@ def clean_text(text_data_df, col):
       text_data_df.iloc[i][col] = text_data_df.iloc[i][col].lower().replace("\n"," ").replace("\t"," ").strip(" ")
       text_data_df.iloc[i][col] = "".join(c for c in text_data_df.iloc[i][col] if c not in punct)
       filtered_words = [w for w in text_data_df[col].iloc[i].split() if w not in stopwords.words('english')]
-      # individual database orinted keywords list is best but due to time constriant could not build it 
+      # individual text database-oriented keywords list is best but due to time constraints could not build it 
       text_data_df.iloc[i][col] = " ".join([c for c in text_data_df[col].iloc[i].split(" ") if not(c[:1].isdigit() and c[1:2] in (p for p in punct))])
       text_data_df.iloc[i][col] = " ".join([w for w in text_data_df[col].iloc[i].split() if w not in stop_words])
       text_data_df.iloc[i][col] = " ".join([w for w in text_data_df[col].iloc[i].split() if w[:-1] not in stop_words])
@@ -15,4 +15,5 @@ def clean_text(text_data_df, col):
       text_data_df.iloc[i][col] = " ".join([w for w in text_data_df[col].iloc[i].split(" ") if not(w[:3].isdigit() and w[3:].isalpha())])
       text_data_df.iloc[i][col] = " ".join([w[:-1] if not(w[:1].isdigit()) and w.endswith(".") else w for w in text_data_df.iloc[i][col].split(" ")])
       text_data_df.iloc[i][col] = " ".join([w for w in text_data_df[col].iloc[i].split(" ") if len(w) > 2 and len(w) < 15])
+      text_data_df.iloc[i][col] = " ".join([w.replace("."," ") if len(w) > 9 or len(w) < 7 else w for w in text_data_df[col].iloc[i].split(" ") ])
   return text_data_df
